eec6bd8375db1f26b91a6cf3f369d5cb5daf18a0
sarthak sachdeva <sarthak.sachdeva.73@gmail.com>
2022-02-17 20:38:27 +0530

    :sparkles: new features
    added tailwind compatibility and basic react player

---
+++ components/react-player/youtubePlayer.tsx
@@ -0,0 +1,16 @@
+import ReactPlayer from "react-player/youtube";
+
+const YoutubePlayer = () => {
+  return (
+    <>
+      <div className="pt-3">
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=43HCYSXZ9GI"
+          controls={true}
+        />
+      </div>
+    </>
+  );
+};
+
+export default YoutubePlayer;
